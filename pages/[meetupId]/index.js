8a10f89ebe5850f4519183b93eaa44f44170f48a
sheng yeong <chia.shengyeong@gmail.com>
2022-11-29 20:51:13 +0800

    passing props and showing classes

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/3/3b/Home_made_sour_dough_bread.jpg"
+      title="First Meetup"
+      description="This is our first meetup"
+      address="5th Street, 12345 City"
+    />
+  );
+}
+
+export default MeetupDetails;
