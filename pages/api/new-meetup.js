60f62cc420493a716d168f449d30c319df6e19ce
sheng yeong <chia.shengyeong@gmail.com>
2022-12-01 09:48:39 +0800

    sending http request with API to mongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,21 +1,21 @@
 import { MongoClient } from "mongodb";
 // /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
     // const { title, image, address, description } = data;
-    const client = MongoClient.connect(`mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.srxozkr.mongodb.net/meetups?retryWrites=true&w=majority`);
+    const client = await MongoClient.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTERNAME}.srxozkr.mongodb.net/meetups?retryWrites=true&w=majority`);
     const db = client.db();
 
     const meetupsCollection = db.collection("meetups");
-    const result = meetupsCollection.insertOne(data);
+    const result = await meetupsCollection.insertOne(data);
 
     console.log(result);
 
-    client.close();
+    client.close(); // without await when insertOne, it causes the following error "error - unhandledRejection: PoolClosedError [MongoPoolClosedError]: Attempted to check out a connection from closed connection pool"
 
-    res.status(201).jason({message: "Meetup inserted!"});
+    res.status(201).json({message: "Meetup inserted!"});
   }
 }
 
